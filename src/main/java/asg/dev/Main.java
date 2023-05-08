ee05aa6030976747b4e034da5f97189f095b9843
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-08 20:52:56 +0800

    leverage characters to check element text data.

--- src/main/java/asg/dev/Main.java
+++ src/main/java/asg/dev/Main.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -34,6 +35,7 @@ public class Main {
 
     static class SAXLocalNameCount extends DefaultHandler {
         HashMap<String, Integer> tags;
+        ArrayList<String> textData;
         public static void processXMLFile(String xmlFileName) throws Exception{
             SAXParserFactory spf = SAXParserFactory.newInstance();
             spf.setNamespaceAware(true);
@@ -50,6 +52,7 @@ public class Main {
         @Override
         public void startDocument() throws SAXException {
             tags = new HashMap<String, Integer>();
+            textData = new ArrayList<String>();
         }
 
         @Override
@@ -62,9 +65,19 @@ public class Main {
         }
 
         @Override
+        public void characters(char[] ch, int start, int length) {
+            String str = new String(ch,start,length);
+            if ( ! ( str.isEmpty() || str.isBlank() ) )  textData.add(str);
+        }
+
+        @Override
         public void endDocument() throws SAXException{
             if (tags.size() > 0 ) {
                 tags.forEach((k,v) -> System.out.println("localName \"" + k + "\" occurs " + v + " time(s)"));
+            }
+
+            if (textData.size() > 0) {
+                textData.forEach(e -> System.out.println("Text: " + e));
             }
         }
 
