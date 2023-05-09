9e698c81cdef53993dc4ad3f450a00cfee053bff
qzhang16 <qzhang16@ra.rockwell.com>
2023-05-09 13:11:43 +0800

    demo DOM and at the same time, found putting the personal.xsd into the project root and the resources will help run in IDE and run by the Uber jar.

--- src/main/java/asg/dev/Main.java
+++ src/main/java/asg/dev/Main.java
@@ -1,13 +1,17 @@
 package asg.dev;
 
 import org.apache.commons.lang3.StringUtils;
+import org.w3c.dom.*;
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -27,7 +31,8 @@ public class Main {
 //            // for you, but you can always add more by pressing Ctrl+F8.
 //            System.out.println("i = " + i);
 //        }
-        SAXLocalNameCount.processXMLFile("personal-schema.xml");
+//        SAXLocalNameCount.processXMLFile("personal-schema.xml");
+        DOMLocalNameCount.processXMLFile("personal-schema.xml");
     }
 
     static class SAXLocalNameCount extends DefaultHandler {
@@ -101,6 +106,225 @@ public class Main {
             }
         }
 
+    }
+
+    static class DOMLocalNameCount implements ErrorHandler {
+        static final String JAXP_SCHEMA_LANGUAGE =
+                "http://java.sun.com/xml/jaxp/properties/schemaLanguage";
+
+        static final String W3C_XML_SCHEMA =
+                "http://www.w3.org/2001/XMLSchema";
+
+        static final String JAXP_SCHEMA_SOURCE =
+                "http://java.sun.com/xml/jaxp/properties/schemaSource";
+        private PrintStream out;
+
+        private int indent;
+
+        private DOMLocalNameCount(PrintStream out) {
+            this.out = out;
+
+        }
+        public static void processXMLFile(String xmlFileName) throws Exception{
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware(true);
+            dbf.setValidating(true);
+
+            //simple XML data processing
+            dbf.setCoalescing(true);
+            dbf.setIgnoringComments(true);
+            dbf.setIgnoringElementContentWhitespace(true);
+            dbf.setExpandEntityReferences(true);
+            dbf.setAttribute(JAXP_SCHEMA_LANGUAGE, W3C_XML_SCHEMA);
+
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            DOMLocalNameCount dln = new DOMLocalNameCount(System.out);
+            db.setErrorHandler(dln);
+            BufferedReader br = new BufferedReader(new InputStreamReader(dln.getClass().getClassLoader().getResourceAsStream(xmlFileName)));
+            org.w3c.dom.Document doc = db.parse(new InputSource(br));
+            dln.echo(doc);
+
+        }
+
+        @Override
+        public void warning(SAXParseException e) throws SAXException {
+            out.println("warning: " + e);
+            throw new SAXException(e);
+        }
+
+        @Override
+        public void error(SAXParseException e) throws SAXException {
+            out.println("error: " +e);
+            throw new SAXException(e);
+        }
+
+        @Override
+        public void fatalError(SAXParseException e) throws SAXException {
+            out.println("fatal-error: " + e);
+            throw new SAXException(e);
+        }
+
+        private void printlnCommon(Node n) {
+            out.print(" nodeName=\"" + n.getNodeName() + "\"");
+
+            String val = n.getNamespaceURI();
+            if (val != null) {
+                out.print(" uri=\"" + val + "\"");
+            }
+
+            val = n.getPrefix();
+
+            if (val != null) {
+                out.print(" pre=\"" + val + "\"");
+            }
+
+            val = n.getLocalName();
+            if (val != null) {
+                out.print(" local=\"" + val + "\"");
+            }
+
+            val = n.getNodeValue();
+            if (val != null) {
+                out.print(" nodeValue=");
+                if (val.trim().equals("")) {
+                    // Whitespace
+                    out.print("[WS]");
+                }
+                else {
+                    out.print("\"" + n.getNodeValue() + "\"");
+                }
+            }
+            out.println();
+        }
+
+        private void outputIndentation() {
+            for (int i = 0; i < indent; i++) {
+                out.print(" ");
+            }
+        }
+
+        private void echo(Node n) {
+            outputIndentation();
+            int type = n.getNodeType();
+
+            switch (type) {
+                case Node.ATTRIBUTE_NODE -> {
+                    out.print("ATTR:");
+                    printlnCommon(n);
+                }
+                case Node.CDATA_SECTION_NODE -> {
+                    out.print("CDATA:");
+                    printlnCommon(n);
+                }
+                case Node.COMMENT_NODE -> {
+                    out.print("COMM:");
+                    printlnCommon(n);
+                }
+                case Node.DOCUMENT_FRAGMENT_NODE -> {
+                    out.print("DOC_FRAG:");
+                    printlnCommon(n);
+                }
+                case Node.DOCUMENT_NODE -> {
+                    out.print("DOC:");
+                    printlnCommon(n);
+                }
+                case Node.DOCUMENT_TYPE_NODE -> {
+                    out.print("DOC_TYPE:");
+                    printlnCommon(n);
+                    NamedNodeMap nodeMap = ((DocumentType) n).getEntities();
+                    indent += 2;
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Entity entity = (Entity) nodeMap.item(i);
+                        echo(entity);
+                    }
+                    indent -= 2;
+                }
+                case Node.ELEMENT_NODE -> {
+                    out.print("ELEM:");
+                    printlnCommon(n);
+                    NamedNodeMap atts = n.getAttributes();
+                    indent += 2;
+                    for (int i = 0; i < atts.getLength(); i++) {
+                        Node att = atts.item(i);
+                        echo(att);
+                    }
+                    indent -= 2;
+                }
+                case Node.ENTITY_NODE -> {
+                    out.print("ENT:");
+                    printlnCommon(n);
+                }
+                case Node.ENTITY_REFERENCE_NODE -> {
+                    out.print("ENT_REF:");
+                    printlnCommon(n);
+                }
+                case Node.NOTATION_NODE -> {
+                    out.print("NOTATION:");
+                    printlnCommon(n);
+                }
+                case Node.PROCESSING_INSTRUCTION_NODE -> {
+                    out.print("PROC_INST:");
+                    printlnCommon(n);
+                }
+                case Node.TEXT_NODE -> {
+                    out.print("TEXT:");
+                    printlnCommon(n);
+                }
+                default -> {
+                    out.print("UNSUPPORTED NODE: " + type);
+                    printlnCommon(n);
+                }
+            }
+
+            indent++;
+            for (Node child = n.getFirstChild(); child != null;
+                 child = child.getNextSibling()) {
+                echo(child);
+            }
+            indent--;
+        }
+
+        public Node findSubNode(String name, Node node) {
+            if (node.getNodeType() != Node.ELEMENT_NODE) {
+                System.err.println("Error: Search node not of element type");
+                System.exit(22);
+            }
+
+            if (! node.hasChildNodes()) return null;
+
+            NodeList list = node.getChildNodes();
+            for (int i=0; i < list.getLength(); i++) {
+                Node subnode = list.item(i);
+                if (subnode.getNodeType() == Node.ELEMENT_NODE) {
+                    if (subnode.getNodeName().equals(name))
+                        return subnode;
+                }
+            }
+            return null;
+        }
+
+        public String getText(Node node) {
+            StringBuffer result = new StringBuffer();
+            if (! node.hasChildNodes()) return "";
+
+            NodeList list = node.getChildNodes();
+            for (int i=0; i < list.getLength(); i++) {
+                Node subnode = list.item(i);
+                if (subnode.getNodeType() == Node.TEXT_NODE) {
+                    result.append(subnode.getNodeValue());
+                }
+                else if (subnode.getNodeType() == Node.CDATA_SECTION_NODE) {
+                    result.append(subnode.getNodeValue());
+                }
+                else if (subnode.getNodeType() == Node.ENTITY_REFERENCE_NODE) {
+                    // Recurse into the subtree for text
+                    // (and ignore comments)
+                    result.append(getText(subnode));
+                }
+            }
+
+            return result.toString();
+        }
 
 
     }
